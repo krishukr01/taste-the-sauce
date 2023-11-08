e9994a5eacea1e484d0e9a96019796e93d01abb2
Krishan Kumar Safi <142893219+krishukr01@users.noreply.github.com>
2023-11-08 17:35:45 +0530

    test cases for sorting algorithem

--- cypress/e2e/sorted-price.cy.js
+++ cypress/e2e/sorted-price.cy.js
@@ -1,5 +1,23 @@
+import 'cypress-map'
+chai.use(require('chai-sorted'))
 describe('sorted price', () => {
-  it('sort item by price', () => {
+  beforeEach(() => {
     cy.visit('/inventory.html')
+  })
+  it('sort item in ascending order', () => {
+    cy.get('.product_sort_container').select('lohi')
+    cy.get('.inventory_item_price')
+      .map('innerText')
+      .mapInvoke('slice', 1)
+      .map(Number)
+      .should('be.ascending')
+  })
+  it('sort item in descending order', () => {
+    cy.get('.product_sort_container').select('hilo')
+    cy.get('.inventory_item_price')
+      .map('innerText')
+      .mapInvoke('slice', 1)
+      .map(Number)
+      .should('be.descending')
   })
 })
